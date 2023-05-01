f017246c2a7966416bdec5186a6f565406d22365
Sahil Darunde <sahildarunde007@gmail.com>
2023-05-01 18:29:41 +0530

    changes made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
         p.product(1,2,3);
         p.product(1.0,2.0);
     }
-    static class Product{
+    public git static class Product{
         public int product(int x, int y) {
             return x+y;
         }
